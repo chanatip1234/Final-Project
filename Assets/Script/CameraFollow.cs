4a7169ac253f1ff3ff18ab2e7af527a3242f6e68
TeerapongMisiri <patterzalol@gmail.com>
2025-11-24 22:36:28 +0700

    cameraFollow

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; // ???????
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+
+    // ???????????
+    public float minX, maxX;
+    public float minY, maxY;
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        // ????????????????
+        float clampedX = Mathf.Clamp(smoothedPosition.x, minX, maxX);
+        float clampedY = Mathf.Clamp(smoothedPosition.y, minY, maxY);
+
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
+    }
+}
