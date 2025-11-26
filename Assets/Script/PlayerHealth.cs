cb1d0de8c94388e0e959470cc8cbce4bdb644df7
chanatip1234 <chanatip.auam@bumail.net>
2025-11-26 23:21:56 +0700

    Player UI health

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -1,22 +1,43 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
 
+    [Header("ส่วนของ UI")]
+    public Slider healthSlider; 
+    public Image fillImage;
     void Start()
     {
         currentHealth = maxHealth;
+
+        if (healthSlider != null)
+        {
+            healthSlider.maxValue = maxHealth;
+            healthSlider.value = maxHealth;
+        }
+        if (fillImage != null) fillImage.enabled = true;
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
-        Debug.Log("โอ๊ย! โดนตี เลือดเหลือ: " + currentHealth);
+        Debug.Log("คุณโดนโจมตี ตอนนี้Hpเหลือ: " + currentHealth);
+       
+        if (healthSlider != null)
+        {
+            healthSlider.value = currentHealth;
 
-        
+            
+            if (fillImage != null)
+            {
+                fillImage.enabled = currentHealth > 0;
+            }
+        }
+
         if (currentHealth <= 0)
         {
             Die();
@@ -25,7 +46,7 @@ public class PlayerHealth : MonoBehaviour
 
     void Die()
     {
-        Debug.Log("ผู้เล่นตายแล้ว...");
+        Debug.Log("คุณตายแล้ว...");
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
